356c593dcfd09d25866c7d17377996b0ebf427cd
Kristoffer Andersson <kod.kristoff@gmail.com>
2024-05-21 08:20:50 +0200

    port: latin lang

--- crates/latin/src/lib.rs
+++ crates/latin/src/lib.rs
@@ -1,1 +1,11 @@
 pub mod types_lat;
+
+#[derive(Debug)]
+pub struct Latin;
+
+impl Language for Latin {
+    // internDict   _ = latinDict
+    // composition  _ = Just $ compDesc
+    // paradigms    _ = foldr insertCommand emptyC commands
+    // wordGuesser  _ = silly_guesser
+}
